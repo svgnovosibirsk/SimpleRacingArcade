33de6ae5c303213058b2f44f0306b8b8c56dbc9f
Sergey <>
2023-11-21 12:49:22 +0400

    initial commit

---
+++ SimpleRacingArcade/SimpleRacingArcade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleRacingArcade
+//
+//  Created by Sergey on 21.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
