248a60177884bf19fdd62daa2b4c0661f3a23ad1
Sergey <>
2023-12-03 10:19:25 +0400

    fix bug with speed setting

--- SimpleRacingArcade/SimpleRacingArcade/Models/GameState.swift
+++ SimpleRacingArcade/SimpleRacingArcade/Models/GameState.swift
@@ -7,40 +7,13 @@
 
 import Foundation
 
-//struct GameStateDTO {
-//    let player: Player
-//    let records: [Int] // Player?
-//}
-
 final class GameState {
     //MARK: - Properties
-    //TODO: make storage via protocol and dependency injection
     private static let storage = UserDefaultsStorage.shared
     static var player = Player(name: "Unknown", car: .red, speed: .normal, obstacle: .picup)
     static var records: [Record] = []
     
-    //TODO: Singleton?
-//    static var shared: GameState = {
-//        let instance = GameState()
-//        return instance
-//    }()
-//
-//    //MARK: - Lifecycle
-//    private init() {}
-    
-    //MARK: - Flow
-//    static func saveState() {
-//        let dto = GameStateDTO(player: player, records: records)
-//        storage.saveGameState(dto: dto)
-//    }
-//
-//    static func fetchState() {
-//        if let dto = storage.fetchGameState() {
-//            player = dto.player
-//            records = dto.records
-//        }
-//    }
-    
+    //MARK: - Methods
     static func saveState() {
         storage.saveGameState()
     }
